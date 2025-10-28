80f0c4ca0e5b6f98eaf766c8c9ba02ae232bdb9d
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-10-28 22:17:47 +0100

    fixed auth fixes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.core.database import Base, engine, get_db
-from routes.auth.routes_auth import routes_auth
+from routes.auth import routes_auth
+from routes.auth.routes_auth import router as auth_router
 
 
 
@@ -9,7 +10,7 @@ app = FastAPI(title="AI LOAN MODEL AND SYSTEM MONITORING")
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(routes_auth.router)
+app.include_router(auth_router, prefix="/auth")
 
 @app.get("/")
 def home(db: Session= Depends(get_db)):
