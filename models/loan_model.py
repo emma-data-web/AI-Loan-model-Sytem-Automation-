7b45e105794a83f5ac23acdb7cc1832d6efdd2ab
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-10-29 12:25:23 +0100

    added the loan scheme table

--- models/loan_model.py
+++ models/loan_model.py
@@ -1,2 +1,15 @@
 from sqlalchemy import Column, Integer,  String
 from app.core.database import Base
+
+
+class User(Base):
+    __table__name = "loan"
+
+    id = Column(Integer, primary_key=True, index=True)
+    applicant = Column(String)
+    amount = Column(float)
+    income = Column(float)
+    credit_score = Column(float)
+    status = Column(String, default="pending")
+    risk_score = Column(float, default=None)
+    
